7bf77fcc92e4ce6b0fa857de92f7d79ed62fc613
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-12 19:55:01 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,24 +1,53 @@
-chrome.contextMenus.onClicked.addListener((a, b) =>
-  b.url[0] != "c" && b.url.slice(8, 23) != "www.youtube.com" && chrome.scripting.executeScript({
-    target: {tabId: b.id, frameIds: [a.frameId]},
-    world: "MAIN",
-    func: ()=> {
-      let u, v, f =e=> {
-        e.stopImmediatePropagation()
-        let k = e.key, n = k == "." ? .016666666666666666 : k == "," ? -.016666666666666666 : 0
-        if (n) {
-          let h = location.href
-          if (!(u == h || v && v.checkVisibility()))
-            if (u = h, h = (v = document.getElementsByTagName("video")).length) {
-              let w = 0, t = 0
-              while (w < (t = v[--h].offsetWidth) && (w = t, k = h), h);
-              v = v[k]
+chrome.contextMenus.onClicked.addListener((info, tab) =>
+    tab.url[0] != "c" &&
+    tab.url.slice(8, 23) != "www.youtube.com" &&
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id, frameIds: [info.frameId] },
+      world: "MAIN",
+      func: () => {
+        let href;
+        let video;
+        let keydownHandler = e => {
+          e.stopImmediatePropagation();
+          let k = e.key;
+          let t =
+            k == "."
+              ? 0.016666666666666666
+              : k == ","
+                ? -0.016666666666666666
+                : 0;
+          if (t) {
+            let _href = location.href;
+            if (!(_href == href || (video && video.checkVisibility()))) {
+              href = _href;
+              let videos = document.getElementsByTagName("video");
+              let i = videos.length;
+              if (i) {
+                let maxWidth = 0;
+                let width = 0;
+                let index = 0;
+                while (
+                  width < (width = videos[--i].offsetWidth) && (maxWidth = width, index = i),
+                  i
+                );
+                video = videos[index];
+              }
             }
-          v && (v.paused || v.pause(), v.currentTime += n)
-        }}
-        removeEventListener("keydown", f)
-        addEventListener("keydown", f)
+            if (video) {
+              video.paused || video.pause();
+              video.currentTime += t;
+            }
+          };
+        }
+        removeEventListener("keydown", keydownHandler);
+        addEventListener("keydown", keydownHandler);
     }
   })
-)
-chrome.runtime.onInstalled.addListener(()=> chrome.contextMenus.create({id: "", title: "Enable stepvf", contexts: ["page","video"]}))
+);
+chrome.runtime.onInstalled.addListener(() =>
+  chrome.contextMenus.create({
+    id: "",
+    title: "Enable stepvf",
+    contexts: ["page", "video"]
+  })
+);

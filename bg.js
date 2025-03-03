f2cb58b3d445f14770975754ac60126c86e73d26
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-03 23:01:48 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -6,44 +6,43 @@
       let isEnable = await chrome.action.getTitle({ tabId }) == "Disable stepvf";
       chrome.action.setIcon({ tabId, path: isEnable ? "off.png" : "on.png" });
       chrome.action.setTitle({ tabId, title: isEnable ?  "Enable stepvf" : "Disable stepvf" });
-      chrome.scripting.executeScript({
-        target: b ? { tabId, frameIds: [a.frameId] } : { tabId },
-        world: (await chrome.contentSettings.javascript.get({
-          primaryUrl: url
-        })).setting == "allow" ? "MAIN" : "ISOLATED",
-        args: [isEnable ? "removeEventListener" : "addEventListener"],
-        func: el => {
-          let href;
-          let video;
-          let f = e => {
-            e.stopImmediatePropagation();
-            let k = e.key;
-            let t = k == "." ? 0.016666666666666666 : k == "," && -0.016666666666666666
-            if (t) {
-              let _href = location.href;
-              if (!(_href == href || (video && video.checkVisibility()))) {
-                href = _href;
-                let videos = document.body.getElementsByTagName("video");
-                let i = videos.length;
-                if (i) {
-                  let index = 0;
-                  if (i > 1) {
-                    let maxWidth = 0;
-                    let width = 0;
-                    while (
-                      maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
-                      i
-                    );
+      try {
+        chrome.scripting.executeScript({
+          target: b ? { tabId, frameIds: [a.frameId] } : { tabId },
+          args: [isEnable ? "removeEventListener" : "addEventListener"],
+          func: el => {
+            let href;
+            let video;
+            let f = e => {
+              e.stopImmediatePropagation();
+              let k = e.key;
+              let t = k == "." ? 0.016666666666666666 : k == "," && -0.016666666666666666
+              if (t) {
+                let _href = location.href;
+                if (!(_href == href || (video && video.checkVisibility()))) {
+                  href = _href;
+                  let videos = document.body.getElementsByTagName("video");
+                  let i = videos.length;
+                  if (i) {
+                    let index = 0;
+                    if (i > 1) {
+                      let maxWidth = 0;
+                      let width = 0;
+                      while (
+                        maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
+                        i
+                    ) ;
+                    }
+                    video = videos[index];
                   }
-                  video = videos[index];
                 }
+                video && (video.paused || video.pause(), video.currentTime += t);
               }
-              video && (video.paused || video.pause(), video.currentTime += t);
             }
+            self[el]("keydown", f);
           }
-          self[el]("keydown", f);
-        } 
-      })
+        })
+      } catch (e) {}
     }
   }
   chrome.action.setTitle({ title: "Enable stepvf" });

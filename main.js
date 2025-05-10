e5b5a85f19d11eea0e223ebb28cf12bbefed7e8e
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-10 14:50:57 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -5,20 +5,23 @@
     chrome.runtime.sendMessage(video.lastChild.remove());
     onkeydown = e => {
       let k = e.keyCode;
-      let t =
+      if (k == 8 || k == 17 || k == 46)
+        return e.preventDefault(video.controls = !video.controls);
+      if (k == 122 && !d.fullscreenElement)
+        return video.requestFullscreen(e.preventDefault());
+      (k = 
           k == 39 ? 5
         : k == 37 ? -5
         : k == 190 ? .03333333333333333
-        : k == 188 ? -.03333333333333333
-        : k == 122 && !d.fullscreenElement;
-      t && (
+        : k == 188 && -.03333333333333333
+      ) && (
         e.preventDefault(),
-        t != !0
-          ? (k > 39 && video.pause(), video.currentTime += t)
-          : video.requestFullscreen()
+        Number.isInteger(k) || video.pause(),
+        video.currentTime += k
       );
     }
-    onmouseup = e => e.button == 3 && d.fullscreenElement && d.exitFullscreen(e.preventDefault());
+    onmouseup = e => e.button == 3 && d.fullscreenElement &&
+      d.exitFullscreen(e.stopImmediatePropagation(e.preventDefault()));
     onwheel = e =>
       video.playbackRate = e.deltaY < 0
         ? Math.min(video.playbackRate + .25, 5)

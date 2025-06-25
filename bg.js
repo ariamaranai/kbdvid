dc64303b19f2af1a236e72404be14e48073a83e9
a <106607480+ariamaranai@users.noreply.github.com>
2025-06-25 22:39:00 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,10 +1,13 @@
-chrome.action.onClicked.addListener(a =>
-  a.url.slice(11, 20) != ".youtube." &&
-  chrome.userScripts.execute({
-    target: { tabId: a.id, allFrames: !0 },
-    js: [{ file: "main.js" }]
-  }).catch(() => 0)
-);
+chrome.action.onClicked.addListener(async a => {
+  if (a.url.slice(11, 20) != ".youtube.") {
+    try {
+      await chrome.userScripts.execute({
+        target: { tabId: a.id, allFrames: !0 },
+        js: [{ file: "main.js" }]
+      })
+    } catch (e) {}
+  }
+});
 chrome.runtime.onUserScriptMessage.addListener((_, s) => {
   let tabId = s.tab.id;
   chrome.action.disable(tabId);
@@ -13,12 +16,19 @@ chrome.runtime.onUserScriptMessage.addListener((_, s) => {
     path: "on.png"
   });
 });
-chrome.runtime.onInstalled.addListener(() => (
-  chrome.userScripts.configureWorld({ messaging: !0 }),
-  chrome.userScripts.register([{
-    id: "0",
-    js: [{ file: "main.js" }],
-    matches: ["https://*/*.mp4*", "file://*.mp4*"],
-    runAt: "document_end"
-  }])
-));
+chrome.runtime.onStartup.addListener(() => {
+  let { userScripts } = chrome;
+  if (userScripts) {
+    userScripts.getScripts(scripts =>
+      scripts.length || (
+        userScripts.configureWorld({ messaging: !0 }),
+        userScripts.register([{
+          id: "0",
+          js: [{ file: "main.js" }],
+          matches: ["https://*/*.mp4*", "file://*.mp4*"],
+          runAt: "document_end"
+        }])   
+      )
+    );
+  }
+});

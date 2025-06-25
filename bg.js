d710b104375fede9aef1871ff7a610776e02c373
a <106607480+ariamaranai@users.noreply.github.com>
2025-06-25 23:49:13 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -16,19 +16,24 @@ chrome.runtime.onUserScriptMessage.addListener((_, s) => {
     path: "on.png"
   });
 });
-chrome.runtime.onStartup.addListener(() => {
-  let { userScripts } = chrome;
-  if (userScripts) {
-    userScripts.getScripts(scripts =>
-      scripts.length || (
-        userScripts.configureWorld({ messaging: !0 }),
-        userScripts.register([{
-          id: "0",
-          js: [{ file: "main.js" }],
-          matches: ["https://*/*.mp4*", "file://*.mp4*"],
-          runAt: "document_end"
-        }])   
-      )
-    );
+{
+  let f = () => {
+    let { userScripts } = chrome;
+    if (userScripts) {
+      userScripts.getScripts(scripts =>
+        scripts.length || (
+          userScripts.configureWorld({ messaging: !0 }),
+          userScripts.register([{
+            id: "0",
+            js: [{ file: "main.js" }],
+            matches: ["https://*/*.mp4*", "file://*.mp4*"],
+            runAt: "document_end"
+          }]),
+          chrome.runtime.onStartup.removeListener(f) 
+        )
+      );
+    }
   }
-});
+  chrome.runtime.onStartup.addListener(f);
+  chrome.runtime.onInstalled.addListener(f);
+}

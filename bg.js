0cd205be44f84e36d2d6bb87f107e50ef4bb38cd
a <106607480+ariamaranai@users.noreply.github.com>
2025-07-23 23:39:08 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -19,20 +19,19 @@ chrome.runtime.onUserScriptMessage.addListener((_, s) => {
 {
   let f = () => {
     let { userScripts } = chrome;
-    if (userScripts) {
-      userScripts.getScripts(scripts =>
-        scripts.length || (
-          userScripts.configureWorld({ messaging: !0 }),
-          userScripts.register([{
-            id: "0",
-            js: [{ file: "main.js" }],
-            matches: ["https://*/*.mp4*", "file://*.mp4*"],
-            runAt: "document_end"
-          }]),
-          chrome.runtime.onStartup.removeListener(f) 
-        )
-      );
-    }
+    userScripts &&
+    userScripts.getScripts(scripts =>
+      scripts.length || (
+        userScripts.configureWorld({ messaging: !0 }),
+        userScripts.register([{
+          id: "0",
+          js: [{ file: "main.js" }],
+          matches: ["https://*/*.mp4*", "file://*.mp4*"],
+          runAt: "document_end"
+        }]),
+        chrome.runtime.onStartup.removeListener(f) 
+      )
+    );
   }
   chrome.runtime.onStartup.addListener(f);
   chrome.runtime.onInstalled.addListener(f);
